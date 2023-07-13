9f193d9cb070fa0b928079300149712bcef97897
yevheniia <eserbina652@gmail.com>
2023-07-13 11:14:18 +0200

    first trying to use husky

---
+++ src/assets/images/BurgerIcon.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const BurgerIcon = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="13"
+            fill="none"
+            viewBox="0 0 18 13"
+        >
+            <path
+                fill="#fff"
+                d="M17.05 5.5H.95a.95.95 0 00-.95.95v.1c0 .525.425.95.95.95h16.1a.95.95 0 00.95-.95v-.1a.95.95 0 00-.95-.95zM17.05 10.5H.95a.95.95 0 00-.95.95v.1c0 .525.425.95.95.95h16.1a.95.95 0 00.95-.95v-.1a.95.95 0 00-.95-.95zM17.05.5H.95a.95.95 0 00-.95.95v.1c0 .525.425.95.95.95h16.1a.95.95 0 00.95-.95v-.1a.95.95 0 00-.95-.95z"
+            ></path>
+        </svg>
+    );
+};
+
+export default BurgerIcon;
---
+++ src/assets/images/CloseButton.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const CloseButton = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="14"
+            height="13"
+            fill="none"
+            viewBox="0 0 14 13"
+        >
+            <path
+                fill="#fff"
+                d="M8.41 6.5l4.3-4.29A1.004 1.004 0 1011.29.79L7 5.09 2.71.79a1.004 1.004 0 00-1.42 1.42l4.3 4.29-4.3 4.29a1 1 0 000 1.42.998.998 0 001.42 0L7 7.91l4.29 4.3a.998.998 0 001.42 0 .997.997 0 00.219-1.095.998.998 0 00-.22-.325L8.41 6.5z"
+            ></path>
+        </svg>
+    );
+};
+
+export default CloseButton;
